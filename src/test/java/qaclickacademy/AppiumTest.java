365f8214d4e6de6760bd2fcb3e284b75cac63a93
Hrushi123-harke <hrushikesh.harke12@gmail.com>
2020-03-23 21:34:14 +0530

    This is my first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAndroidAPI()
+	{
+		System.out.println("NativeAndroidAPI");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+}
